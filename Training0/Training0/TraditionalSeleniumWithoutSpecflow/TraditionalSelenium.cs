907b94291ce3783cd6371fbd25a8fd0cbcd41a29
modupe <modupebashorun@yahoo.co.uk>
2019-03-06 11:17:34 +0000

    atahis is my new code for the training on 5th March 2019 end to end selenium

---
+++ Training0/Training0/TraditionalSeleniumWithoutSpecflow/TraditionalSelenium.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Training0.TraditionalSeleniumWithoutSpecflow
+{
+    [TestClass]
+    public class TraditionalSelenium
+    {
+        [TestMethod]
+        public void BlueSkyFormValidation()
+        {
+            //1. Create Instance of the browser
+            IWebDriver Driver;
+
+            //2. Open the Browser
+            Driver = new ChromeDriver();
+
+            //3. Do your Test
+
+
+            //3a) Navigate website
+            Driver.Navigate().GoToUrl("http://qa1.giftrt.com/community/create");
+            //3a i) Maximize the window
+            Driver.Manage().Window.Maximize();
+
+            //3b) Enter a test into textbox
+
+
+            //3b i)Inspect your element
+            //Done with f12 Developer Tool
+            //3b ii)Decide on the attribute to use to find your element
+            //Decide to ID
+            //3b iii)Copy the attribute
+            //nav-signin-tab
+            Driver.FindElement(By.Id("nav-signin-tab")).Click();
+            //user_email
+            //3b iv)Wrie the code to find the element
+            //Driver.FindElement(By.Id("user_email"));
+            //3b v)Perform the action (ie Type into textbox)
+            //enter Email Address 
+            Driver.FindElement(By.Id("user_email")).SendKeys("b_bimpe@yahoo.co.uk");
+
+            //enter the Password
+            Driver.FindElement(By.Id("user_password")).SendKeys("Jawdan21");
+
+
+            //3c) Select from a drop list
+
+
+            //3d) Click a Sign In button
+           
+
+            IWebElement radio = Driver.FindElement(By.Id("btn_signin"));
+            radio.Click();
+            //*[@id='btn_signin']
+            //Driver.FindElement(By.ClassName("font-size-15")).Displayed);
+
+            //Assert.IsTrue(Driver.FindElement(By.ClassName("font-size-15")).Displayed);
+
+           //Assert.IsTrue(Driver.FindElement(By.ClassName("font-size-15")).Displayed, "The name is not displayed");
+
+
+
+            //4) Close Browser
+            Driver.Close();
+        }
+    }
+}
---
+++ Training0/Training0/TraditionalSeleniumWithoutSpecflow/ValidationOfForms.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Training0.TraditionalSeleniumWithoutSpecflow
+{
+    [TestClass]
+    public class ValidationOfForms
+    {
+        [TestMethod]
+        public void ValidateTest()
+        {
+            
+            IWebDriver Driver;
+
+            Driver = new ChromeDriver();
+
+            
+
+
+            
+            Driver.Navigate().GoToUrl("http://qa1.giftrt.com/community/create");
+            
+            Driver.Manage().Window.Maximize();
+
+            
+            Driver.FindElement(By.Id("nav-signin-tab")).Click();
+            
+            Driver.FindElement(By.Id("user_email")).SendKeys("b_bimpe@yahoo.co.uk");
+
+            
+            Driver.FindElement(By.Id("user_password")).SendKeys("Jawdan21");
+
+
+            
+            Driver.FindElement(By.Id("btn_signin")).Click();
+
+            //Driver.FindElement(By.ClassName("nav-link")).Click();
+
+            //1Thread.Sleep(5000);
+
+            //Implicit waits
+            //Tells you webdriver to pull down for certain amount of time in order to find the element
+            //By default it is set to zero
+
+            //When do I need to wait/ When is it required
+
+            //a)For a page to load
+            //b)For a java script or ajax to finish
+            //c)When new form or message is displayed/upload on a form
+
+            //2 Implicit waits
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            //3 Explicit wait
+            //WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+
+            //Func<IWebDriver, bool> waitForElent = new Func<IWebDriver, bool>(IWebDriver web) =>
+                {
+                //web.FindElement(By.ClassName("font-size-15")).Displayed));
+                //return true;
+            }
+                
+   
+
+         
+
+           
+
+            Assert.IsTrue(Driver.FindElement(By.ClassName("font-size-15")).Displayed);
+
+            Assert.IsTrue(Driver.FindElement(By.ClassName("font-size-15")).Displayed, "The name is not displayed");
+
+            //Waiting
+
+            //Thread.Sleep(5000)
+
+            //Explicit and Implicit wait
+
+            //Fluents wait
+
+            Driver.Close();
+
+        }
+    }
+}
